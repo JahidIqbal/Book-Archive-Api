77cda6aece7542ebab9dbb5299da27d3eab7ff7c
hrid620 <hridishere@gmail.com>
2021-09-02 18:09:11 +0600

    if condition left

--- Book.js
+++ Book.js
@@ -18,16 +18,16 @@ const displaySearchResult = books => {
     // console.log(books);
     const searchResult = document.getElementById('search-result');
     for (const book of first10Book) {
+        console.log(book);
         const div = document.createElement('div')
-        const authorInfo = book.author_name[0];
         div.classList.add('col');
         div.innerHTML = `<div class="card h-100 w-50">
-       
         <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid" alt="...">
         <div class="card-body">
-                <h4 class="card-title">${book.title}</h
-                
-            <p class="card-text"></p>
+                <h4 class="card-title fs-4 mb-2 ">${book.title}</h> 
+            <p class="card-text fs-6">Author:${book.author_name[0]} </p>
+            <p class="card-text fs-6">Publisher:${book.publisher[0]} </p>
+            <p class="card-text fs-6">First Published:${book.first_publish_year} </p>
         </div>
     </div>
         `
